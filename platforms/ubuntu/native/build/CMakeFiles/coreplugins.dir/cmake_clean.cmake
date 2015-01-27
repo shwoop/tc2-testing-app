3fb3d5abb448e441efe744e78366bb16dc93a2a7
Samuel Colvin <s@muelcolvin.com>
2015-01-27 15:02:59 +0000

    initial build

---
+++ platforms/ubuntu/native/build/CMakeFiles/coreplugins.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "cordova-ubuntu_automoc.cpp"
+  "cordovaubuntuplugin_automoc.cpp"
+  "coreplugins_automoc.cpp"
+  "CMakeFiles/coreplugins.dir/src/coreplugins.cpp.o"
+  "CMakeFiles/coreplugins.dir/coreplugins_automoc.cpp.o"
+  "libcoreplugins.pdb"
+  "libcoreplugins.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/coreplugins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
